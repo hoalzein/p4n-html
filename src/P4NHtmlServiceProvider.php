de1137c752404af0fa968370b157dbac43be776b
hoalzein <hashim.omar.zein@hotmail.com>
2020-04-07 16:50:48 +0200

    Configuration merging integrated.

--- src/P4NHtmlServiceProvider.php
+++ src/P4NHtmlServiceProvider.php
@@ -7,15 +7,15 @@ use Illuminate\Support\ServiceProvider;
 class P4NHtmlServiceProvider extends ServiceProvider {
 
     public function register() {
-        //$this->mergeConfigFrom(__DIR__ . '/config/p4n-html.php', 'p4n-html');
+        $this->mergeConfigFrom(__DIR__ . '/config/p4n-html.php', 'p4n-html');
     }
 
     public function boot() {
-        $this->publishes([__DIR__ . '/config/p4n-html.php' => config_path('p4n-html.php'),], 'config');
+        $this->publishes([__DIR__ . '/config/p4n-html.php' => config_path('p4n-html.php'),], 'p4n-config');
+        $this->publishes([__DIR__ . '/assets' => public_path('prof4net'),], 'p4n-assets');
         //$this->publishes([__DIR__ . '/resources/views' => base_path('resources/views/vendor/p4n-html'),], 'views');
         //$this->publishes([__DIR__ . '/database/migrations/' => database_path('migrations')], 'migrations');
         //$this->publishes([__DIR__ . '/path/to/translations' => resource_path('lang/vendor/courier'),], 'translations');
-        $this->publishes([__DIR__.'/assets' => public_path('prof4net'),], 'assets');
         //$this->loadMigrationsFrom(__DIR__.'/path/to/migrations');
         //$this->loadRoutesFrom(__DIR__.'/routes.php');
         //$this->loadTranslationsFrom(__DIR__.'/path/to/translations', 'courier');
